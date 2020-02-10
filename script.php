f053a005ed3c4612be955f083a32aa408d6767bb
Paulo Antonio <paulo.werder@gmail.com>
2020-02-10 09:50:26 -0300

    antes das 10

--- script.php
+++ script.php
@@ -1,25 +1,38 @@
-<?php
-    $json = file_get_contents('questoes.json');           
-    $quest = json_decode($json);
-    $acerto = 0;
-    foreach($quest as $cont => $q)
-    {
-        $res_user = $_POST[$cont];
-        $res_corr = isset($q->correct) ? $q->correct : null;
-        if($res_corr == $res_user)
-        {
-            $acerto+=1;
-        }
-    }
+<html>
+    <head>    
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+        <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+        <link rel="stylesheet" href="main.css">
+    </head>
+    <body>
+        <div class="card centered jumbotron container pt-3 my-4" style="width:330px; ">
+            <?php
+                $json = file_get_contents('questoes.json');           
+                $quest = json_decode($json);
+                $acerto = 0;
+                foreach($quest as $cont => $q)
+                {
+                    $res_user = $_POST[$cont];
+                    $res_corr = isset($q->correct) ? $q->correct : null;
+                    if($res_corr == $res_user)
+                    {
+                        $acerto+=1;
+                    }
+                }
 
-    if (intval($acerto) >= 7)
-    {
-        echo "Aprovado \n";
-    } else 
-    {
-        echo "Retido \n";
-    }
+                if (intval($acerto) >= 7)
+                {
+                    echo "Situação do aluno: Aprovado \n";
+                } else 
+                {
+                    echo "Situação do aluno: Retido \n";
+                }
 
-
-    echo $acerto;
-?>
+                echo "<br>";
+                echo "Numero de questões corretas: ".$acerto."/10";
+            ?>
+        </div>    
+    </body>
+</html>
--- teste.php
+++ teste.php
@@ -4,15 +4,14 @@
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    <link rel="stylesheet" href="main.css">
     </head>
     <body>
         <form method="POST" action="script.php">
             <?php
             
                 $arr = array('a', 'b', 'c', 'd', 'e');
-                $img = array("", "./img/1.jpg", "./img/2.jpg", "./img/3.jpg", "./img/4.jpg", "./img/5.jpg", "./img/6.jpg", "./img/7.jpg", "./img/8.jpg", "./img/9.jpg", "./img/10.jpg");
-                $json = file_get_contents('questoes.json');
-                
+                $json = file_get_contents('questoes.json');                
                 $quest = json_decode($json);
             
                 $cont = 1;
@@ -21,7 +20,7 @@
                 foreach ($quest as $q)
                 {
             ?>
-                    <div class="card" style="width:400px">
+                    <div class="card centered" style="width:400px;">
                         <div class="card-body">
                             <?php
                             
@@ -33,15 +32,13 @@
                                 foreach($q->alternativas as $alt)
                                 {
                                     //imprimindo perguntas e as letras
-                                    echo '<input type="radio" name="'.$cont.'" value="'.$arr[$num].'">';
+                                    echo '<input type="radio" name="'.$cont.'" value="'.$arr[$num].'" required>';
                                     echo $arr[$num].". $alt <br>";
                                     $num+=1;
                                 }
             
                                 $num=0;
                                 $img= "./img/".$cont.".jpg";
-                                echo "(Correta: $q->correct)";
-                                echo "<br>";
                                                             
                             ?>       
                         </div>    
