3bd55d361e2b2a988e0635436559abcdba02757e
Paulo Antonio <paulo.werder@gmail.com>
2020-02-09 23:28:38 -0300

    arrumando o perfil imagem para cada questão

--- teste.php
+++ teste.php
@@ -1,10 +1,16 @@
 <html>
+    <head>
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css">
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.4.1/jquery.min.js"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.16.0/umd/popper.min.js"></script>
+    <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js"></script>
+    </head>
     <body>
         <form method="POST" action="script.php">
             <?php
             
                 $arr = array('a', 'b', 'c', 'd', 'e');
-
+                $img = array("", "./img/1.jpg", "./img/2.jpg", "./img/3.jpg", "./img/4.jpg", "./img/5.jpg", "./img/6.jpg", "./img/7.jpg", "./img/8.jpg", "./img/9.jpg", "./img/10.jpg");
                 $json = file_get_contents('questoes.json');
                 
                 $quest = json_decode($json);
@@ -14,27 +20,39 @@
 
                 foreach ($quest as $q)
                 {
-                    //imprimindo o enunciado e o numero de questao
-                    echo "<br>";
-                    echo "$cont. $q->enunciado ";
-                    echo "<br>";
+            ?>
+                    <div class="card" style="width:400px">
+                        <div class="card-body">
+                            <?php
+                            
+                                //imprimindo o enunciado e o numero de questao
+                                echo "<br>";
+                                echo "$cont. $q->enunciado ";
+                                echo "<br>";
+                        
+                                foreach($q->alternativas as $alt)
+                                {
+                                    //imprimindo perguntas e as letras
+                                    echo '<input type="radio" name="'.$cont.'" value="'.$arr[$num].'">';
+                                    echo $arr[$num].". $alt <br>";
+                                    $num+=1;
+                                }
             
-                    foreach($q->alternativas as $alt)
-                    {
-                        //imprimindo perguntas e as letras
-                        echo '<input type="radio" name="'.$cont.'" value="'.$arr[$num].'">';
-                        echo $arr[$num].". $alt <br>";
-                        $num+=1;
-                    }
-
-                    $num=0;
-
-                    echo "(Correta: $q->correct)";
-                    echo "<br>";
-                    
-                    $cont+=1;
+                                $num=0;
+                                $img= "./img/".$cont.".jpg";
+                                echo "(Correta: $q->correct)";
+                                echo "<br>";
+                                                            
+                            ?>       
+                        </div>    
+                        <?php
+                            echo '<img class="card-img-bottom" src="./img/'.$cont.'.jpg" style="width:100%">';
+                            $cont+=1;                 
+                        ?>
+                    </div>
+            <?php
                 }
-            ?>   
+            ?> 
             <input type="submit" value="enviar">     
         </form>
     </body>
