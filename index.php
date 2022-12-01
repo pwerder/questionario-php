d54a4f6f341f26b2a16d7b46729c1e3e4ea640dd
Paulo Antonio <paulo.werder@gmail.com>
2022-11-30 21:41:51 -0300

    inciando nova estrutura

--- index.php
+++ index.php
@@ -1,21 +1,26 @@
-<html>
+<!DOCTYPE html>
+<html lang="pt-br">
     <head>
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/css/bootstrap.min.css" integrity="sha384-Vkoo8x4CGsO3+Hhxv8T/Q5PaXtkKtu6ug5TOeNV6gBiFeWPGFN9MuhOf23Q9Ifjh" crossorigin="anonymous">
-        <script src="https://code.jquery.com/jquery-3.4.1.slim.min.js" integrity="sha384-J6qa4849blE2+poT4WnyKhv5vZF5SrPo0iEjwBvKU7imGFAV0wwj1yYfoRSJoZ+n" crossorigin="anonymous"></script>
-        <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js" integrity="sha384-Q6E9RHvbIyZFJoft+2mJbHaEWldlvI9IOYy5n3zV9zzTtmI3UksdQRVvoxMfooAo" crossorigin="anonymous"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.4.1/js/bootstrap.min.js" integrity="sha384-wfSDF2E50Y2D1uUdj0O3uMBJnjuUD4Ih7YwaYd1iqfktj0Uod8GCExl3Og8ifwB6" crossorigin="anonymous"></script>
-        <link rel="stylesheet" href="main.css">
+        <title>Inciar Teste</title>
+        <!-- Reset CSS -->
+        <link rel="stylesheet" href="./css/reset.css">
+        <!-- Fonte Ubuntu -->
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+        <link href="https://fonts.googleapis.com/css2?family=Ubuntu:wght@400;700&display=swap" rel="stylesheet">
+        <!-- Meus estilos-->
+        <link rel="stylesheet" href="./css/style.css">
     </head>
     <body>
-        <div class="jumbotron container pt-3 my-4" style="height:250px; width:300px; margin:40%;">
-            <h1 style="text-align:center;">Login</h1>
-            Nome: <input type="text" placeholder="Seu nome completo" class="input-group mb-3" required>
-            <br>
-            <a href="teste.php">
-            <button type="button" class="btn btn-outline-secondary btn-block">INICIAR</button>
-            </a>
-        </div>   
+        <main>
+            <form class="login">
+                <legend>Inciar Teste</legend>
+    
+                <input class="name" type="text" placeholder="Seu nome*" required>
+                <button>Começar</button>
+            </form>
+        </main>
     </body>
 </html>
